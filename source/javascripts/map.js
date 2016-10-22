321ec7b8a82c091f53cb823060388b074defaf0e
Thomas Chrétien <thomas@chretien.co>
2016-10-22 13:57:47 +0800

    working in train

--- source/javascripts/map.js
+++ source/javascripts/map.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.map-full-size').on('click', function() {
     $('.map').toggleClass('is-full-size')
-    if($('.map-full-size').text() == 'CLOSE') {
+    if($('.map-full-size').text() == 'COLLAPSE') {
       $('.map-full-size').text('FULL SIZE')
       $('.map-full-size').removeClass('is-colored')
     } else {
-      $('.map-full-size').text('CLOSE')
+      $('.map-full-size').text('COLLAPSE')
       $('.map-full-size').addClass('is-colored')
     }
   })
